a3f01fb9a297292968689b9fcd0fa18c8e14b501
shoqqan <tataev.shokan@gmail.com>
2023-08-16 02:02:07 +0600

    fix navigations

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes,Navigate} from "react-router-dom";
 import React from "react";
 import {Restaurant} from "./pages/RestorauntComponent/Restaurant";
-import {Landing} from "./pages/Landing/Landing";
 import './i18n'
 import {OrdersRequestingPage} from "./pages/OrdersRequestingPage";
 
@@ -10,7 +9,7 @@ export const App = () => {
     return (
             <div className={'bg-amber-50 w-screen h-screen flex justify-center items-center overflow-y-auto'}>
             <Routes>
-                <Route path={'/'} element={<Landing/>}/>
+                <Route path={'/'} element={<Navigate to={'restaurant'}/>}/>
                 <Route path={'/restaurant'} element={<Restaurant/>}/>
                 <Route path={'/restaurant/request/:orderIds'} element={<OrdersRequestingPage/>}/>
             </Routes>
